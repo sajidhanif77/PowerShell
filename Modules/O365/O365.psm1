99b65019eaaafcfc8a0da4d65a5760ff3fc176c6
sajidhanif77 <sajidhanif77@outlook.com>
2016-10-20 11:20:37 +0200

    Added Code

--- Modules/O365/O365.psm1
+++ Modules/O365/O365.psm1
@@ -7,3 +7,12 @@ Function AssignAlumniExchangeLicense ($user) {
     Set-MsolUser -UserPrincipalName $user -UsageLocation $UsageLocation 
     Set-MsolUserLicense -UserPrincipalName $user -AddLicenses $AccountSkuId 
 }
+
+Function GiveFullAccessRights ($mailbox, $user){
+    Add-MailboxPermission -Identity $mailbox -User $user -AccessRights FullAccess -Automapping:$false
+}
+
+
+Function GiveSendAsRights ($mailbox, $user){
+    Add-RecipientPermission -Identity $mailbox -Trustee $user -AccessRights SendAs -Confirm:$false
+}
