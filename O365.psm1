6029323ac75b7e94af6e9488744f1abc3c9723f7
sajidhanif77 <sajidhanif77@outlook.com>
2016-10-20 11:16:20 +0200

    Delete unnecessary copy

--- O365.psm1
+++
@@ -1,9 +0,0 @@
-Function AssignAlumniExchangeLicense ($user) {
-    $UsageLocation = “FR”
-    $AccountSkuId = “aupedu:EXCHANGESTANDARD_ALUMNI”
-    $disabledPlans +="YAMMER_EDU" 
-    $disabledPlans +="INTUNE_O365"
-    $LicenseOptions = New-MsolLicenseOptions –AccountSkuId $AccountSkuId -DisabledPlans $disabledPlans
-    Set-MsolUser -UserPrincipalName $user -UsageLocation $UsageLocation 
-    Set-MsolUserLicense -UserPrincipalName $user -AddLicenses $AccountSkuId 
-}
