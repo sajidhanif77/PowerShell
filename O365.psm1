ce57733f0f0214ab60fbeca0ab3fb8336b973014
sajidhanif77 <sajidhanif77@outlook.com>
2016-10-20 11:12:45 +0200

    Modules

---
+++ O365.psm1
@@ -0,0 +1,9 @@
+Function AssignAlumniExchangeLicense ($user) {
+    $UsageLocation = “FR”
+    $AccountSkuId = “aupedu:EXCHANGESTANDARD_ALUMNI”
+    $disabledPlans +="YAMMER_EDU" 
+    $disabledPlans +="INTUNE_O365"
+    $LicenseOptions = New-MsolLicenseOptions –AccountSkuId $AccountSkuId -DisabledPlans $disabledPlans
+    Set-MsolUser -UserPrincipalName $user -UsageLocation $UsageLocation 
+    Set-MsolUserLicense -UserPrincipalName $user -AddLicenses $AccountSkuId 
+}
